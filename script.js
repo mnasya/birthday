60129a56c9463892de279c9189c1aaad72bfb22c
mnasya <nastya.zakharova200116@gmail.com>
2025-12-02 20:38:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ function generateHearts() {
     heart.innerHTML = '❤️';
 
     // Расположение по всей странице
-    heart.style.left = Math.random() * 100 + 'vw'; // по горизонтали (виндоверс)
-    heart.style.top = Math.random() * 100 + 'vh'; // по вертикали (вьюпорт-вер)
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.top = Math.random() * 100 + 'vh';
 
     heart.style.animationDelay = Math.random() * 2 + 's';
 
     document.body.appendChild(heart);
 
-    // Удаляет сердечко через 3 секунды
     setTimeout(() => {
       if (heart.parentNode) {
         heart.parentNode.removeChild(heart);
       }
     }, 3000);
   }
+
 }
